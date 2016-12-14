9d1d009f8296f6941e78fc1eb30eff3a7cb77566
Ly Duc Long <longlyduc@gmail.com>
2016-12-14 15:27:36 +0000

    singup - login -logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def devise_error_messages!
+    resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
--- db/migrate/20161213114246_devise_create_users.rb
+++ db/migrate/20161213114246_devise_create_users.rb
@@ -12,11 +12,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :chatword
       t.integer :role
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       t.string :confirmation_token
       t.datetime :confirmed_at
