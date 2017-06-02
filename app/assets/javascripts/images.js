662ea4c07cfa392bbc93b251c9ceeeb94888cdae
quoc viet <quocvietlee@gmail.com>
2017-06-02 08:31:41 +0700

    fix-form

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,11 +1,15 @@
 $(function(){ Portfolio.init(); });
-$(document).ready(function() {
-$('.thumb').click(function(){
+  $(document).ready(function() {
+    $('.thumb').click(function(){
       $('.modal-body-image').empty();
-    var title = $(this).parent('a').attr("title");
-    $('.modal-title').html(title);
-    $($(this).parents('div').html()).appendTo('.modal-body-image');
-    $(".modal-body-image").find('span').remove();
-    $('#myModalImage').modal({show:true});
-});
+      var title = $(this).parent('a').attr("title");
+      $('.modal-title').html(title);
+      $($(this).parents('div').html()).appendTo('.modal-body-image');
+      $(".modal-body-image").find('span').remove();
+      $('#myModalImage').modal({show:true});
+    });
+  $('ul.first').bsPhotoGallery({
+    "classes" : "col-md-6 col-sm-3 col-xs-4 col-xxs-12",
+    "hasModal" : true
+  });
 });
