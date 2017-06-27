f5bc652b7e2bef81330b19f0324120d93cd273ca
nvtanh <nvtanh.ptit@gmail.com>
2017-06-27 13:32:43 +0700

    add log

--- bin/cisetup.sh
+++ bin/cisetup.sh
@@ -3,5 +3,6 @@
 gem install --no-document pronto pronto-rubocop pronto-brakeman pronto-reek pronto-rails_best_practices pronto-eslint
 
 echo run review
+echo ${GITHUB_ACCESS_TOKEN} - ${CI_PULL_REQUEST##*/}
 PRONTO_GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN} PRONTO_PULL_REQUEST_ID=${CI_PULL_REQUEST##*/} pronto run -f github_pr -c origin/develop
 # fi
