86fb7a54aca515fbc2b3b0908bc6dc94f8de3a2f
ngocthinh <tnngocthinh4@gmail.com>
2017-06-27 20:06:49 +0700

    fix sort clubs

--- app/assets/javascripts/libs/pro_ui/other_clubs.js
+++ app/assets/javascripts/libs/pro_ui/other_clubs.js
@@ -14,9 +14,15 @@ $(document).on('turbolinks:load', function() {
   $('.stype-organizations-other-clubs').on('change',function(e) {
     SearchOtherClub();
   });
+
+  $('.sort-other-clubs').on('change',function(e) {
+    SearchOtherClub();
+  });
 });
 
 function SearchOtherClub() {
+  var sort_club = $('.sort-other-clubs').val()
+  var sort_params = LoadSortParam(sort_club);
   var search = $('.form-search-other-clubs').val()
   var stype_clubs = $('.stype-other-clubs').val();
   var rate_value = $( ".range-slider-demo" ).val();
@@ -24,9 +30,21 @@ function SearchOtherClub() {
   var rate_gteq = rate[0];
   var rate_lteq = rate[1];
   var stype_organizations = $('.stype-organizations-other-clubs').val();
-  var data = {q: {name_or_content_cont: search,
-    club_type_eq: stype_clubs, organization_id_eq: stype_organizations,
+  var data = {q: {s: [sort_params, sort_club], name_or_content_cont: search,
+    club_type_eq: stype_clubs,organization_id_eq: stype_organizations,
     rating_gteq: rate_gteq, rating_lteq: rate_lteq}}
   $.get('/clubs', data , null, 'script');
   return false;
+}
+
+function LoadSortParam(sort_club) {
+  var sort_params = '';
+  if(sort_club == 'name asc'){
+    sort_params = 'name asc';
+  } else if (sort_club == 'rating desc'){
+    sort_params = "rating desc";
+  } else if (sort_club == 'created_at desc'){
+    sort_params = 'created_at asc';
+  }
+  return sort_params;
 }
