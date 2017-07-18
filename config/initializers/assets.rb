c7ced7ab8bcdbc1cafc38e7d30d21e6353f8e67c
ngocthinh <tnngocthinh4@gmail.com>
2017-07-18 08:43:33 +0700

    fix login+organization_UI

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(application_admin.js ckeditor/config.js images.js
   club_manager/select_tag.js manager/select_tag.js chosen.js bootstrap-tagsinput.js handle_menu.js
   close.js jquery.range.js)
-Rails.application.config.assets.precompile += %w(application_admin.css user_login.scss
+Rails.application.config.assets.precompile += %w(application_admin.css user_login.scss user_login_modal.scss
   organization-details.css intro.css jquery.range.css user_profile)
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
